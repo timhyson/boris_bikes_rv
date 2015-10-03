992d4e653b056f79b68c0f874ed6d7250b79f0b8
Tim Hyson <tjhyson@gmail.com>
2015-10-03 18:45:48 +0100

    refactoring into private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,29 @@
 require_relative 'bike'
 
 class DockingStation
+  DEFAULT_CAPACITY = 20
+
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking Station full' if @bikes.count >= 20
+    fail 'Docking Station full' if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bikes.empty?
   end
 end
