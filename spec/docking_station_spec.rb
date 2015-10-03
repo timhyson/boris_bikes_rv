936c490f1c9e2c4dc7ac7b1b650048dc583153ad
Tim Hyson <tjhyson@gmail.com>
2015-10-03 17:48:59 +0100

    added working bike released test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,4 +6,9 @@ describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to(:release_bike)
   end
+
+  it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
