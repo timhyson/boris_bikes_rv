713b7b84d059ddab7a49089499d72444ca3779c6
Tim Hyson <tjhyson@gmail.com>
2015-10-03 18:36:34 +0100

    docking station has a capacity of 20

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,17 @@
 require_relative 'bike'
 
 class DockingStation
-  def dock(bike)
-    fail 'Docking Station full' if @bike
-    @bike = bike
+  def initialize
+    @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' unless @bike
-    @bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
+
+  def dock(bike)
+    fail 'Docking Station full' if @bikes.count >= 20
+    @bikes << bike
   end
 end
