fecf2556c5830c9969a45bf6d284f963a7ecc967
Tim Hyson <tjhyson@gmail.com>
2015-10-04 11:36:22 +0100

    doubles introduced

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -29,9 +29,13 @@ describe DockingStation do
     end
 
     it 'releases working bikes' do
-      subject.dock(Bike.new)
-      bike = subject.release_bike
-      expect(bike).to be_working
+      bike = double :bike, working?: true
+      subject.dock(bike)
+      expect(subject.release_bike).to be(bike)
+    end
+
+    it 'raises an error if there are no bikes available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
 
     it 'does not release broken bikes' do
@@ -45,13 +49,9 @@ describe DockingStation do
   describe '#dock' do
     it { is_expected.to respond_to(:dock).with(1).argument }
 
-    it 'raises an error if there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
-    end
-
     it 'raises an error when full' do
-      subject.capacity.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error 'Docking Station full'
+      subject.capacity.times { subject.dock(double :bike) }
+      expect { subject.dock(double :bike) }.to raise_error 'Docking Station full'
     end
   end
 
